3ee334d481fde92c2e15c9ea1d9b3bd56c012895
sempol <sempo@lol.com>
2020-08-20 13:58:17 +0300

    try fix fonts

--- pages/_document.js
+++ pages/_document.js
@@ -12,14 +12,10 @@ class MyDocument extends Document {
           <Head>
             <title>StrikeBy - информация о забастовках в РБ</title>
             <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Roboto"
-            />
-            <link
               href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
               rel="stylesheet"
             />
-            <style global jsx>{`
+            <style>{`
               * {
                 box-sizing: border-box;
               }
