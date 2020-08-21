cb8962731b7f67038c8742d5bd5c1c9ca49dbfea
sempol <sempo@lol.com>
2020-08-21 16:16:23 +0300

    add favicon

--- components/PageTemplate.js
+++ components/PageTemplate.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 import Header from "../components/Header";
+import Head from "next/head";
 
 const PageTemplate = ({ children }) => (
   <>
+    <Head>
+      <title>StrikeBy - информация о забастовках в Беларуси</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <Header />
     {children}
   </>
-)
+);
 
-export default PageTemplate
+export default PageTemplate;
